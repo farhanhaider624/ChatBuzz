a465fb984d486cffe4201a9e24b1ef3c3af37036
farhanhaider624 <farhanhaider624@gmail.com>
2024-08-29 01:06:48 +0530

    create group chat api added

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat, fetchChats } = require("../controllers/chatControllers");
+const { accessChat, fetchChats, createGroupChat } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
-// router.route('/group').get(protect, createGroupChat);
+router.route('/group').post(protect, createGroupChat);
 // router.route('/rename').get(protect, renameGroup);
 // router.route('/groupRemove').get(protect, removeFromGroup);
 // router.route('/groupAdd').get(protect, addToGroup);
