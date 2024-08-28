63de565334118472ee937cb1e1f1e0895dd4a8ef
farhanhaider624 <farhanhaider624@gmail.com>
2024-08-28 22:42:26 +0530

    fetch chats api added

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { protect } = require("../middleware/authMiddleware");
-const { accessChat } = require("../controllers/chatControllers");
+const { accessChat, fetchChats } = require("../controllers/chatControllers");
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat);
-// router.route('/').get(protect, fetchChats);
+router.route('/').get(protect, fetchChats);
 // router.route('/group').get(protect, createGroupChat);
 // router.route('/rename').get(protect, renameGroup);
 // router.route('/groupRemove').get(protect, removeFromGroup);
